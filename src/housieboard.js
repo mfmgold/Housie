17b396fec8625f4cdcd9db10ca2bdb4a5790ed9a
Murtuza Masalawala <mfmgold@gmail.com>
2021-07-18 10:19:36 +1000

    Full screen issue resolved Added QR code feature for Housie Ticket Cleanup

--- src/housieboard.js
+++ src/housieboard.js
@@ -1,4 +1,4 @@
-let version = '2.0.3'; // update version here. 
+let version = '2.0.4'; // update version here. 
 
 Number.prototype.pad = function(size) {
     var s = String(this);
@@ -17,6 +17,7 @@ let tktnum = String.fromCharCode(getRndInteger(65, 90)) + String.fromCharCode(ge
 document.getElementById('footer').innerHTML = " Board # " + tktnum + " generated on: " + dateTime + ", (c) Murtuza Masalawala - ver: " + version;
 document.getElementById('button').innerHTML = 'PLAY';
 
+var fullScreen = () => !window.screenTop && !window.screenY; // arrow function to check if window is full screen. 
 
 var canvas, context;
 var padx = pady = 10;
@@ -26,22 +27,32 @@ for (idx = 0; idx < 90; idx++) {
     checked[idx] = false;
 }
 var lastNumber = document.getElementById('lastnum');
-
+var sizeQRCode=256;
+var isQRDisplayed=false;
 Resize();
 
 function Resize() {
-    // setting canvas size. 
+    // setting canvas size.   
     canvas = document.getElementById('myCanvas');
     context = canvas.getContext('2d');
-    var screenWidth = document.documentElement.clientWidth - 10;
-    var screenHeight = document.documentElement.clientHeight - 80; // consider size of button and footer. 
+    //if (!fullScreen()) goFullScreen(); 
+    let screenWidth = document.documentElement.clientWidth - 10;
+    let screenHeight = document.documentElement.clientHeight - 80; // consider size of button and footer. 
     context.canvas.width = screenWidth;
     context.canvas.height = screenHeight;
     document.getElementById('button').style = "height:50px;width:300px;font-size: 24px;background-color: #19B7F1;border: 2px solid #008CBA;position:absolute;left:" + (((document.documentElement.clientWidth / 2 - 150) / document.documentElement.clientWidth) * 100) + "%;top:" + (((screenHeight / document.documentElement.clientHeight) * 100) + 1) + "%";
+    document.getElementById('buttonQR').style = "height:50px;width:300px;font-size: 24px;background-color: #19B7F1;border: 2px solid red;position:absolute;left:" + (((document.documentElement.clientWidth-320) / document.documentElement.clientWidth) * 100) + "%;top:" + (((screenHeight / document.documentElement.clientHeight) * 100) + 1) + "%";
 
-    document.getElementById('popupbox').style = "height:" + screenHeight / 4 + "px;width:" + screenWidth / 4 + "px;font-size: 72px;background-color: yellow;border: 2px solid #008CBA;position:absolute;left:        " + (((document.documentElement.clientWidth / 2 - (screenWidth / 8)) / document.documentElement.clientWidth) * 100) + "%; top:" + (((document.documentElement.clientHeight / 2 - (screenHeight / 8)) / document.documentElement.clientHeight) * 100) + "%";
+    // PopupBox settings
+    document.getElementById('popupbox').style = "height:" + screenHeight / 4 + "px;width:" + screenWidth / 4 + "px;font-size: 72px;background-color: yellow;border: 2px solid #008CBA;position:absolute;left:" + (((document.documentElement.clientWidth / 2 - (screenWidth / 8)) / document.documentElement.clientWidth) * 100) + "%; top:" + (((document.documentElement.clientHeight / 2 - (screenHeight / 8)) / document.documentElement.clientHeight) * 100) + "%";
     document.getElementById('popupbox').innerHTML = 'Hello!';
-    $("#popupbox").fadeOut(10); // make it vanish immedaitely
+    $("#popupbox").fadeOut(1); // make it vanish immediately
+
+    // PopupBoxQR settings
+    sizeQRCode = screenWidth<screenHeight ?  Math.trunc(screenWidth/4-20):Math.trunc(screenHeight/4-20);
+    document.getElementById('popupboxQR').style = "height:" + (sizeQRCode + 20) + "px;width:" + (sizeQRCode + 20) + "px;font-size: 72px;background-color: white;border: 2px solid #008CBA;position:absolute;left:" + (((document.documentElement.clientWidth / 2 - ((sizeQRCode + 20) / 2)) / document.documentElement.clientWidth) * 100) + "%; top:" + (((document.documentElement.clientHeight / 2 - ((sizeQRCode + 20) / 2)) / document.documentElement.clientHeight) * 100) + "%";
+    displayHousieTicketQRCode();
+    $("#popupboxQR").fadeOut(1); // make it vanish immediately
 
     cellw = (screenWidth - 2 * padx) / 10;
     cellh = (screenHeight - 2 * pady) / 9;
@@ -67,39 +78,41 @@ function Resize() {
             context.fillText(idx, x + z, y + cellh - 15);
         }
     }
-
 }
 
 canvas.onclick = function(event) {
-    // get canvas size and position
-    let boundingRect = canvas.getBoundingClientRect();
-    // translate mouse event coordinates to canvas coordinates
-    let x = (event.clientX - boundingRect.left) * (canvas.width / boundingRect.width);
-    let y = (event.clientY - boundingRect.top) * (canvas.height / boundingRect.height);
+    if (!fullScreen()) goFullScreen();
+    else {
+        if (isQRDisplayed) hidePopupboxQR(); /// Ensure QR is out of the way when Numbers are being punched manually.
+        // get canvas size and position
+        let boundingRect = canvas.getBoundingClientRect();
+        // translate mouse event coordinates to canvas coordinates
+        let x = (event.clientX - boundingRect.left) * (canvas.width / boundingRect.width);
+        let y = (event.clientY - boundingRect.top) * (canvas.height / boundingRect.height);
 
-    let idx;
-    // mark selected cell
-    if (context.isPointInPath(x, y)) {
-        let row = Math.trunc((x - padx) / cellw);
-        let col = Math.trunc((y - pady) / cellh);
-        let x1 = row * cellw + padx;
-        let y1 = col * cellh + pady;
-        idx = col * 10 + row + 1;
-        window.speechSynthesis.speak(new SpeechSynthesisUtterance(idx));
-        if (checked[idx]) {
-            context.fillStyle = 'white';
-        } else {
-            context.fillStyle = 'yellow';
+        let idx;
+        // mark selected cell
+        if (context.isPointInPath(x, y)) {
+            let row = Math.trunc((x - padx) / cellw);
+            let col = Math.trunc((y - pady) / cellh);
+            let x1 = row * cellw + padx;
+            let y1 = col * cellh + pady;
+            idx = col * 10 + row + 1;
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(idx));
+            if (checked[idx]) {
+                context.fillStyle = 'white';
+            } else {
+                context.fillStyle = 'yellow';
+            }
+            context.fillRect(x1 + 2, y1 + 2, cellw - 4, cellh - 4);
+            context.fillStyle = 'black';
+            z = (cellw - context.measureText(idx).width) / 2;
+            context.fillText(idx, x1 + z, y1 + cellh - 15);
+            checked[idx] = !checked[idx];
+            checked[idx] ? ++countMarkedCells : --countMarkedCells; // if cell is unmarked then reduce the count
+            lastNumber.innerHTML = 'Last number was : ' + idx;
         }
-        context.fillRect(x1 + 2, y1 + 2, cellw - 4, cellh - 4);
-        context.fillStyle = 'black';
-        z = (cellw - context.measureText(idx).width) / 2;
-        context.fillText(idx, x1 + z, y1 + cellh - 15);
-        checked[idx] = !checked[idx];
-        checked[idx] ? ++countMarkedCells : --countMarkedCells; // if cell is unmarked then reduce the count
-        lastNumber.innerHTML = 'Last number was : ' + idx;
     }
-
 };
 
 function getRndInteger(min, max) {
@@ -110,9 +123,12 @@ function PlayPause() {
     let change = document.getElementById('button');
     if (change.innerHTML == 'PLAY') {
         change.innerHTML = 'PAUSE';
+        document.getElementById('buttonQR').disabled = true;
+        if (isQRDisplayed) hidePopupboxQR();       
         PlayNextNumber();
     } else {
         change.innerHTML = 'PLAY';
+        document.getElementById('buttonQR').disabled = false;
         PauseNextNumber();
     }
 }
@@ -176,6 +192,47 @@ function PlayNextNumber() {
 function PauseNextNumber() {
     clearTimeout(t);
     timer_is_on = false;
+}
+
+function goFullScreen() { 
+    let elem = document.documentElement;
+    if (elem.requestFullscreen) {    
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {      
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { 
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {  
+        elem.msRequestFullscreen();
+    } 
+}
+
+function displayHousieTicketQRCode() {
+    // QR code for housie ticket, using qrcode.js in src directory   
+    if (isQRDisplayed) {
+        hidePopupboxQR();
+    } else {
+        isQRDisplayed=true;
+        document.getElementById("popupboxQR").innerHTML="";
+        new QRCode(document.getElementById("popupboxQR"), {
+            text: "http://housieticket.ml",
+            width: sizeQRCode,
+            height: sizeQRCode,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });  
+        $("#popupboxQR").fadeIn();
+    }
+}
+
+function hidePopupboxQR() {
+    isQRDisplayed=false;
+    $("#popupboxQR").fadeOut(1000); //fade out in 1 second
+}
+
+function hidePopupbox() {
+    $("#popupbox").fadeOut(5000); //fade out in 5 seconds
 }
 
 window.addEventListener("keypress", evt => {
